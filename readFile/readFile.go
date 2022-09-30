fb0d484d02d22b6857f3bf0b752f1ce9e75b70cb
arthurkyambadde <arthurkyambadde9@gmail.com>
2022-09-30 07:44:55 +0300

    add: create Readfile function
    This function is responsible for reading data from .txt file

--- readFile/readFile.go
+++ readFile/readFile.go
@@ -1,5 +1,20 @@
 package readfile
 
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
 func readfile(fileName string) {
 
+	fileContent, err := ioutil.ReadFile(fileName)
+
+	if err != nil {
+		log.Fatalf("%q has not been read", err)
+	}
+
+	fileData := string(fileContent)
+
+	fmt.Println(fileData)
 }
