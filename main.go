9b8fb49867a89f964b58cc5938b48f795ad22843
arthurkyambadde <arthurkyambadde9@gmail.com>
2022-10-20 23:41:14 +0300

    fix: linter rules

--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
 	gethttprequest "webscrapper/getHttpRequest"
 )
 
 func main() {
 	// readfile.Readfile("dummy.txt")
 	gethttprequest.Gethttprequest()
-	fmt.Println("hey am working")
 }
